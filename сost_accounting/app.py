822796fb0a971075ee19f0b26fb3ffa7a267b1a5
Serega <ser.pisarenko@gmail.com>
2024-04-22 16:46:17 +0300

    Доходы и расходы. Меню авторизации.

--- сost_accounting/app.py
+++ сost_accounting/app.py
@@ -1,6 +1,51 @@
-# TODO опишите здесь функции для решения задачи
+"""
+Данное приложение осуществляет учет и хранение информации о доходах и расходах зарегистрированных пользователей.
+"""
+import os
+from typing import Never
+import string
+
+HELLO_TXT_1 = 'Программа учёта доходов и расходов.'
+HELLO_TXT_2 = 'Для работы с программой необходимо войти или зарегистрироваться.'
+HELLO_MENU = [
+    '1 - Войти со своим логином и паролем.\n',
+    '2 - Зарегистрировать нового пользователя.\n',
+    '3 - Выйти из программы.']
+
+
+def authorization_menu() -> int:
+    print(f"{'*' * (len(HELLO_TXT_2) + 4)}\n"
+          f"* {HELLO_TXT_1:^{len(HELLO_TXT_2)}} *\n"
+          f"* {HELLO_TXT_2} *\n"
+          f"{'*' * (len(HELLO_TXT_2) + 4)}"
+          )
+    while True:
+        print(''.join(HELLO_MENU))
+
+        hello_menu_item = input("Выберите действие: ")
+        # os.system('cls' if os.name == 'nt' else 'clear')
+        if not hello_menu_item.isdigit() or int(hello_menu_item) > 3 or int(hello_menu_item) < 1:
+            print("Необходимо ввести номер пункта 1, 2 или 3. Повторите ввод.")
+            continue
+        break
+
+    return int(hello_menu_item)
+
+
+def input_username(text: str):
+    input(f"{text}\nВведите имя пользователя: ")
+
+
+def main() -> Never:
+    match authorization_menu():
+        case 1:
+            input_username('* Вход в программу *')
+        case 2:
+            input_username('* Регистрация пользователя *')
+        case 3:
+            exit()
+
 
 
 if __name__ == '__main__':
-    # TODO запустите здесь все необходимые функции
-    ...
+    main()
