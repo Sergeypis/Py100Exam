56b1960441ecc309a9d4d34e68fdc4c1170fa800
Serega <ser.pisarenko@gmail.com>
2024-04-25 14:45:23 +0300

    Доходы и расходы. refactoring branch. patch-1.

--- сost_accounting/app.py
+++ сost_accounting/app.py
@@ -2,14 +2,13 @@
 Данное приложение осуществляет учет и хранение информации о доходах и расходах зарегистрированных пользователей.
 """
 import os
-from pprint import pprint
-from prettytable import PrettyTable
-import pickle
 import json
-# from typing import Never, Optional
-from typing import Optional
-import string
 import copy
+import string
+from typing import Optional
+
+import pickle
+from prettytable import PrettyTable
 
 
 HELLO_TXT_1 = 'Программа учёта доходов и расходов.'
@@ -31,7 +30,11 @@ user_filename = 'user.json'
 data_filename = 'data.pkl'
 
 
-def authorization_menu() -> int:
+def authorization_menu() -> str:
+    """
+    Функция вывода меню авторизации пользователя. Включаетв себя валидацию введённых значений.
+    :return: Строка с номером пункта меню.
+    """
     print(f"{'*' * (len(HELLO_TXT_2) + 4)}\n"
           f"* {HELLO_TXT_1:^{len(HELLO_TXT_2)}} *\n"
           f"* {HELLO_TXT_2} *\n"
@@ -47,10 +50,14 @@ def authorization_menu() -> int:
             continue
         break
 
-    return int(hello_menu_item)
+    return hello_menu_item
 
 
-def main_menu() -> int:
+def main_menu() -> str:
+    """
+    Функция вывода главного меню программы. Включаетв себя валидацию введённых значений.
+    :return: Строка с номером пункта меню.
+    """
     print(f"{'*' * (len(MAIN_MENU_HD) + 20)}\n"
           f"* {MAIN_MENU_HD:^{len(MAIN_MENU_HD) + 16}} *\n"
           f"{'*' * (len(MAIN_MENU_HD) + 20)}"
@@ -65,7 +72,7 @@ def main_menu() -> int:
             continue
         break
 
-    return int(main_menu_item)
+    return main_menu_item
 
 
 def read_data_file(filename: str) -> Optional[dict]:
@@ -82,8 +89,62 @@ def read_data_file(filename: str) -> Optional[dict]:
         exit()
 
 
-def create_new_user_data(username: str):
-    new_user_data_dict = {
+def write_data_file(filename: str, data: dict) -> None:
+    """
+    Функция сохраняет данные пользователей сериализованные в Pickle файл, если он существует.
+    :param filename: Имя/путь до файла
+    :param data: Словарь с данными пользователей
+    :return: Ничего не возвращает
+    """
+    try:
+        with open(filename, 'wb') as pkl_file:
+            pickle.dump(data, pkl_file)
+    except OSError:
+        print("Ошибка файла данных!!! Обратитесь к разработчику. Программа завершена.")
+        exit()
+
+
+def input_description(text: str) -> str:
+    """
+    Функция валидации введённой пользователем строки описания/имени пользователя.
+    Требуется ввести непустую строку и не пробелы.
+    :return: Строка описания.
+    """
+    text_input_dialog = text
+    while True:
+        input_value = input(text_input_dialog)
+        if input_value.isspace() or input_value == '':
+            text_input_dialog = "Недопустимый ввод!!! Повторите попытку: "
+            continue
+        return input_value
+
+
+def input_amount_money(text: str) -> str:
+    """
+    Функция валидации введённых пользователем значений. Требуется ввести только целые числа или float.
+    :return: Строка  числа, округленного до 2х знаков после запятой.
+    """
+    text_input_dialog = text
+    input_value = ""
+    while True:
+        try:
+            input_value = input(text_input_dialog)
+            input_value = round(float(input_value), 2)
+            return str(input_value)
+        except (ValueError, TypeError):
+            print(f"{input_value}: Неверный ввод!")
+            text_input_dialog = "Введите сумму в формате: 123 руб. или 123.45 руб.: "
+            continue
+
+
+def create_new_user_data(username: str) -> None:
+    """
+    Функция записывает структуру данных нового пользователя в бинарный Pickle файл. Создаёт файл при его отсутствии.
+    :param username: Имя текущего пользователя/ login
+    :return: Ничего не возвращает
+    """
+    user_data = dict()
+    new_user_data_dict = {  # Структура данных для хранения расходов/доходов текущего пользователя
         'расходы': [
             # {'стол': '12000'},
             # {'стул': '3500'},
@@ -94,37 +155,43 @@ def create_new_user_data(username: str):
             # {'продажа': '74000'}
         ]
     }
-    user_data = dict()
 
-    income_amount_money = input(f"{username}, вы успешно зарегистрированы в программе. "
-                                f"Введите сумму для учета в 'Доходах': ")
-    income_description = input("Ведите описание дохода: ")
+    income_amount_money = input_amount_money("Введите сумму для учета в 'Доходах': ")
+    income_description = input_description("Ведите описание дохода: ")
 
-    user_data = read_data_file(data_filename)
+    if os.path.exists(data_filename):
+        user_data = read_data_file(data_filename)
 
     user_data[username] = new_user_data_dict
     user_data[username]['доходы'].append({income_description: income_amount_money})
 
-    with open(data_filename, 'wb') as pkl_file:
-        pickle.dump(user_data, pkl_file)
-
-    return float(income_amount_money)
+    write_data_file(data_filename, user_data)
+    print(f"{'*-*-*-' * 10}\n{username}, ваш балланс на текущий момент: {float(income_amount_money):.2f} руб.")
 
 
 def check_username(username: str) -> Optional[dict]:
+    """
+    Функция проверяет наличие пользовательских данных (имени пользователя) в файле .json
+    :param username: Имя, введённое пользователем при входе или регистрации.
+    :return: Словарь с логином и паролем текущего пользователя.
+    """
     try:
         with open(user_filename, 'r', encoding='utf-8') as f:
             dict_users = json.load(f)
             list_user_auth_data = [user for user in dict_users.get('users') if user.get('login') == username]
 
             return None if not list_user_auth_data else list_user_auth_data[0]
-            # return list_user_auth_data[0]
 
     except (FileNotFoundError, json.JSONDecodeError):
         return None
 
 
-def add_user(username: str) -> list:
+def add_user_json(username: str) -> dict:
+    """
+    Функция добавляет и сохраняет в файле .json данные для авторизации новых пользователей (логин/пароль).
+    :param username: Имя, введённое пользователем при регистрации.
+    :return: Словарь с логином и паролем последнего сохранённого пользователя.
+    """
     user_data_access = dict()
     user_data_access['users'] = []
     userpass = input("Введите пароль: ")
@@ -140,51 +207,59 @@ def add_user(username: str) -> list:
             user_data_access['users'].append({'login': username, 'password': userpass})
             json.dump(user_data_access, file, indent=4)
 
-    # create_new_user_data(user_data_access['users'][-1])
     return user_data_access['users'][-1]
 
 
-def entry_user(text: str):
+def entry_user() -> (dict, bool):
+    """
+    Функция обработчик перврго пункта меню авторизации.Вход в программу по логину и паролю. Производится валидация
+    введённых данных, проверка регистрации пользователя, сверка пароля.
+    :return: Кортеж: Словарь данных для авторизации текущего пользователя (логин/пароль). Флаг авторизации.
+    """
     authorization = False
     count_answer = 0
-    username = input(f"{text}\nВведите имя пользователя: ")
-    user_auth_data = check_username(username)
-    if (user_auth_data is None) or (not user_auth_data):
-        while count_answer < 3:
-            answer = input(
-                f"Отсутствуют данные о пользователе '{username}'. Создать нового? Y-Да, N-Нет (Выход): ").lower()
-            match answer:
-                case 'y':
-                    return add_user(username), authorization
-                case 'n':
-                    exit()
-                case _:
-                    print("Неверный ввод!!!")
-                    count_answer += 1
-                    continue
-        exit()
-    elif user_auth_data:
-        authorization = True
-        count_pass = 0
-        while count_pass < 3:
-            userpass = input("Введите пароль: ")
-            if userpass == user_auth_data.get('password'):
-                print("super")
-                return user_auth_data, authorization
-            print("Неверный пароль!!! попробуйте еще раз.")
-            count_pass += 1
-            continue
-        print("Неудачная авторизация. Программа завершена!")
-        exit()
+
+    while True:
+        name_input = input_description("* Вход в программу *\nВведите имя пользователя: ")
+        user_auth_data = check_username(name_input)
+        if (user_auth_data is None) or (not user_auth_data):
+            while True:
+                answer = input(f"Отсутствуют данные о пользователе '{name_input}', выберите действие:\n"
+                               f"R - ввести заново, N - создать нового , Q - выйти из программы: ").lower()
+                match answer:
+                    case 'r':
+                        break
+                    case 'n':
+                        return add_user_json(name_input), authorization
+                    case 'q':
+                        exit()
+                    case _:
+                        print("Неверный ввод!!!")
+                        count_answer += 1
+                        if count_answer == 2:
+                            exit()
+                        continue
+        elif user_auth_data:
+            authorization = True
+            count_pass = 0
+            while count_pass < 3:
+                userpass = input("Введите пароль: ")
+                if userpass == user_auth_data.get('password'):
+                    return user_auth_data, authorization
+                print("Неверный пароль!!! попробуйте еще раз.")
+                count_pass += 1
+                continue
+            print("Неудачная авторизация. Программа завершена!")
+            exit()
 
 
-def reg_user(text: str) -> list:
+def reg_user(text: str) -> dict:
     count_answer = 0
     while True:
         username = input(f"{text}\nВведите имя пользователя: ")
         user_auth_data = check_username(username)
         if (user_auth_data is None) or (not user_auth_data):
-            return add_user(username)
+            return add_user_json(username)
         elif user_auth_data:
             while True:
                 answer = input(
@@ -202,17 +277,24 @@ def reg_user(text: str) -> list:
                         continue
 
 
-def authorization_menu_handler():
+def authorization_menu_handler() -> (dict, bool):
+    """
+    Функция обработчик меню авторизации пользователя. Производится авторизация или регистрация нового пользователя.
+    :return: Кортеж: Словарь данных текущего пользователя. Флаг авторизации.
+    """
+    current_user = dict()
+    authorization = False
+
     match authorization_menu():
-        case 1:
-            current_user, authorization = entry_user('* Вход в программу *')
-            return current_user, authorization
-        case 2:
+        case '1':  # Вход с логином и паролем
+            current_user, authorization = entry_user()
+        case '2':  # Регистрация нового пользователя
             current_user = reg_user('* Регистрация пользователя *')
-            authorization = False
-            return current_user, authorization
-        case 3:
+        case '3':  # Выход из программы
+            print("Программа завершена.")
             exit()
+
+    return current_user, authorization
 
 
 def save_pkl_user_data(user_data: dict):
@@ -235,9 +317,9 @@ def output_user_data(username: str, user_data: dict):
 
     expenses = current_user_data.get('расходы')
     incomes = current_user_data.get('доходы')
-    total_expenses = sum([int(*item.values()) for item in expenses])
-    total_incomes = sum([int(*item.values()) for item in incomes])
-    summ_table_row = ['Итого расходы:', total_expenses, 'Итого доходы:', total_incomes]
+    total_expenses = sum([float(*item.values()) for item in expenses])
+    total_incomes = sum([float(*item.values()) for item in incomes])
+    summ_table_row = ['Итого расходы:', f'{total_expenses:.2f}', 'Итого доходы:', f'{total_incomes:.2f}']
 
     smaller_list = min(expenses, incomes, key=len)
     bigger_list = max(expenses, incomes, key=len)
@@ -250,37 +332,38 @@ def output_user_data(username: str, user_data: dict):
     output_table.add_rows(table_list)
     output_table.add_row(['', '', '', ''])
     output_table.add_row(summ_table_row)
+    print(f"{'*-*-*-' * 10}\nТаблица учёта Расходов и Доходов пользователя '{username}':")
     print(output_table)
 
 
 def main_menu_handler(username: str):
-    user_data = read_data_file()
+    user_data = read_data_file(data_filename)
     current_user_data = user_data[username]
     while True:
         match main_menu():
-            case 1:  # Добавить расходы
-                expenses_amount_money = input(f"{username}, введите сумму для учета в 'Расходах': ")
-                expenses_description = input("Ведите описание расхода: ")
+            case '1':  # Добавить расходы
+                expenses_amount_money = input_amount_money(f"{username}, введите сумму для учета в 'Расходах': ")
+                expenses_description = input_description("Ведите описание расхода: ")
                 current_user_data['расходы'].append({expenses_description: expenses_amount_money})
 
-            case 2:  # Добавить доходы
-                income_amount_money = input(f"{username}, введите сумму для учета в 'Доходах': ")
-                income_description = input("Ведите описание дохода: ")
+            case '2':  # Добавить доходы
+                income_amount_money = input_amount_money(f"{username}, введите сумму для учета в 'Доходах': ")
+                income_description = input_description("Ведите описание дохода: ")
                 current_user_data['доходы'].append({income_description: income_amount_money})
 
-            case 3:  # Удалить строку расходов
-                del_expenses_name = input(f"{username}, введите название 'Расхода' для удаления: ")
+            case '3':  # Удалить строку расходов
+                del_expenses_name = input_description(f"{username}, введите название 'Расхода' для удаления: ")
                 for expense in current_user_data['расходы']:
                     if del_expenses_name in expense:
                         current_user_data['расходы'].remove(expense)
 
-            case 4:  # Удалить строку доходов
-                del_income_name = input(f"{username}, введите название 'Дохода' для удаления: ")
+            case '4':  # Удалить строку доходов
+                del_income_name = input_description(f"{username}, введите название 'Дохода' для удаления: ")
                 for income in current_user_data['доходы']:
                     if del_income_name in income:
                         current_user_data['доходы'].remove(income)
 
-            case 5:  # Выход из программы
+            case '5':  # Выход из программы
                 print("Программа завершена.")
                 exit()
         output_user_data(username, current_user_data)
@@ -292,12 +375,19 @@ def main() -> None:
     current_user, authorization = authorization_menu_handler()  # Авторизация и регистрация пользователя
     username = current_user.get('login')
     if not authorization:
-        cash_new_user = create_new_user_data(username)  # Создание файла данных и добавление в него нового пользователя
-        print(f"{'*-*-*-' * 10}\n{username}, ваш балланс на текущий момент: {cash_new_user:.2f} руб.")
+        print(f"{username}, вы успешно зарегистрированы в программе. ")
+        create_new_user_data(username)  # Создание файла данных и добавление в него нового пользователя
     else:
-        print(f"Таблица учёта Расходов и Доходов пользователя '{username}':")
-        user_data = read_data_file()
-        output_user_data(username, user_data[username])
+        if os.path.exists(data_filename):
+            user_data = read_data_file(data_filename)
+            if username in user_data:
+                output_user_data(username, user_data[username])
+            else:
+                print(f"{username}, ваши данные были ранее кем-то повреждены, начните учёт заново.")
+                create_new_user_data(username)
+        else:
+            print(f"{username}, ваши данные были ранее кем-то повреждены, начните учёт заново.")
+            create_new_user_data(username)
 
     main_menu_handler(username)
 
